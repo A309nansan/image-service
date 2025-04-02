152fde479fbc691a11d51cc132070a327570889e
전성호 <kopasd99@gmail.com>
2025-04-02 11:04:03 +0900

    [FIX] : RequestPart -> RequestParam

--- src/main/java/site/nansan/global/controller/ImageController.java
+++ src/main/java/site/nansan/global/controller/ImageController.java
@@ -3,6 +3,7 @@ package site.nansan.global.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,8 +18,8 @@ public class ImageController {
     private final ImageService imageService;
 
     @PostMapping("/upload")
-    public ResponseEntity<String> uploadImage(@RequestPart("childId") String childId,
-                                              @RequestPart("localDateTime") LocalDateTime localDateTime,
+    public ResponseEntity<String> uploadImage(@RequestParam("childId") String childId,
+                                              @RequestParam("localDateTime") LocalDateTime localDateTime,
                                               @RequestPart("file") MultipartFile file) {
 
         String hashedFileName = imageService.uploadImage(childId, localDateTime.toString(), file);
