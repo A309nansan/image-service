6a406f33292a751cd95a65ec2f22410a97535365
전성호 <kopasd99@gmail.com>
2025-04-02 15:57:24 +0900

    [FEAT] : 이미지 request, 숫자 request dto 추가

--- src/main/java/site/nansan/global/controller/ImageController.java
+++ src/main/java/site/nansan/global/controller/ImageController.java
@@ -1,19 +1,13 @@
 package site.nansan.global.controller;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.*;
+import site.nansan.global.dto.UploadImageRequest;
+import site.nansan.global.dto.UploadNumberImageRequest;
 import site.nansan.global.service.ImageService;
 
-import java.time.LocalDateTime;
-
-@Validated
 @RestController
 @RequiredArgsConstructor
 public class ImageController {
@@ -21,24 +15,26 @@ public class ImageController {
     private final ImageService imageService;
 
     @PostMapping("/upload")
-    public ResponseEntity<String> uploadImage(
-            @RequestParam("childId") @NotNull(message = "childId is required") String childId,
-            @RequestParam("localDateTime") @NotNull(message = "localDateTime is required") LocalDateTime localDateTime,
-            @RequestPart("file") @NotNull(message = "file is required") MultipartFile file) {
+    public ResponseEntity<String> uploadImage(@Valid @ModelAttribute UploadImageRequest request) {
 
-        String hashedFileName = imageService.uploadImage(childId, localDateTime.toString(), file);
+        String hashedFileName = imageService.uploadImage(
+                request.getChildId(),
+                request.getLocalDateTime().toString(),
+                request.getFile()
+        );
 
         return ResponseEntity.ok(hashedFileName);
     }
 
     @PostMapping("/upload/number")
-    public ResponseEntity<String> uploadNumberImage(
-            @RequestParam("childId") @NotNull(message = "childId is required") String childId,
-            @RequestParam("localDateTime") @NotNull(message = "localDateTime is required") LocalDateTime localDateTime,
-            @RequestParam("number") @NotNull(message = "number is required")  Integer number,
-            @RequestPart("file") @NotNull(message = "file is required") MultipartFile file) {
+    public ResponseEntity<String> uploadNumberImage(@Valid @ModelAttribute UploadNumberImageRequest request) {
 
-        String hashedFileName = imageService.uploadImage(childId, localDateTime.toString(), number, file);
+        String hashedFileName = imageService.uploadImage(
+                request.getChildId(),
+                request.getLocalDateTime().toString(),
+                request.getNumber(),
+                request.getFile()
+        );
 
         return ResponseEntity.ok(hashedFileName);
     }
---
+++ src/main/java/site/nansan/global/dto/UploadNumberImageRequest.java
@@ -0,0 +1,24 @@
+package site.nansan.global.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+
+@Data
+public class UploadNumberImageRequest {
+    @NotNull(message = "childId is required")
+    private String childId;
+
+    @NotNull(message = "localDateTime is required")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime localDateTime;
+
+    @NotNull(message = "number is required")
+    private Integer number;
+
+    @NotNull(message = "file is required")
+    private MultipartFile file;
+}
