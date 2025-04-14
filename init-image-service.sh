5cee623ff95e3dd896713042eea890726139d541
octopus-kim <62287654+octopus-kim@users.noreply.github.com>
2025-04-14 16:37:55 +0900

    Create init-image-service.sh

---
+++ init-image-service.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# 명령어 실패 시 스크립트 종료
+set -euo pipefail
+
+# 로그 출력 함수
+log() {
+  echo "[$(date +'%Y-%m-%d %H:%M:%S')] $*"
+}
+
+# 에러 발생 시 로그와 함께 종료하는 함수
+error() {
+  log "Error on line $1"
+  exit 1
+}
+
+trap 'error $LINENO' ERR
+
+log "스크립트 실행 시작."
+
+# docker network 생성
+if docker network ls --format '{{.Name}}' | grep -q '^nansan-network$'; then
+  log "Docker network named 'nansan-network' is already existed."
+else
+  log "Docker network named 'nansan-network' is creating..."
+  docker network create --driver bridge nansan-network
+fi
+
+# 필요한 환경변수를 Vault에서 가져오기
+log "Get credential data from vault..."
+
+TOKEN_RESPONSES=$(curl -s --request POST \
+  --data "{\"role_id\":\"${ROLE_ID}\", \"secret_id\":\"${SECRET_ID}\"}" \
+  https://vault.nansan.site/v1/auth/approle/login)
+
+CLIENT_TOKEN=$(echo "$TOKEN_RESPONSES" | jq -r '.auth.client_token')
+
+SECRET_RESPONSE=$(curl -s --header "X-Vault-Token: ${CLIENT_TOKEN}" \
+  --request GET https://vault.nansan.site/v1/kv/data/auth)
+
+CONFIG_SERVER_URI=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.configserver.private.uri')
+CONFIG_SERVER_USERNAME=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.configserver.username')
+CONFIG_SERVER_PASSWORD=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.configserver.password')
+MINIO_ACCESS_KEY=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.minio.username')
+MINIO_SECRET_KEY=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.minio.password')
+
+# Build Gradle
+log "build gradle"
+./gradlew clean build
+
+# 기존 서비스 삭제
+log "image-service undeploy"
+docker rm -f image-service
+
+# 기존 image-service 이미지를 삭제하고 새로 빌드
+log "image-service image remove and build."
+docker rmi image-service:latest || true
+docker build -t image-service:latest .
+
+# Docker로 image-service 서비스 실행
+log "Execute image-service..."
+docker run -d \
+  --name image-service \
+  --restart unless-stopped \
+  -e CONFIG_SERVER_URI=${CONFIG_SERVER_URI} \
+  -e CONFIG_SERVER_USERNAME=${CONFIG_SERVER_USERNAME} \
+  -e CONFIG_SERVER_PASSWORD=${CONFIG_SERVER_PASSWORD} \
+  -e MINIO_ACCESS_KEY=${MINIO_ACCESS_KEY} \
+  -e MINIO_SECRET_KEY=${MINIO_SECRET_KEY} \
+  --network nansan-network \
+  image-service:latest
+
+echo "==== Succeed!!! ===="
