a7ebbb013e6c214a67ade65a88409ebf04a95066
전성호 <kopasd99@gmail.com>
2025-05-09 12:51:35 +0900

    [CONFIG]: MINIO 경로 수정

--- init-image-service.sh
+++ init-image-service.sh
@@ -71,7 +71,7 @@ docker run -d \
   -e RABBITMQ_USERNAME=${RABBITMQ_USERNAME} \
   -e RABBITMQ_PASSWORD=${RABBITMQ_PASSWORD} \
   -e DEFAULT_EUREKA_URL=http://eureka:8761/eureka \
-  -e MINIO_API_URL=https://s3.nansan.site \
+  -e MINIO_API_URL=http://minio:9000 \
   -e MINIO_ACCESS_KEY=${MINIO_ACCESS_KEY} \
   -e MINIO_SECRET_KEY=${MINIO_SECRET_KEY} \
   --network nansan-network \
--- src/main/java/site/nansan/global/util/MinioUtil.java
+++ src/main/java/site/nansan/global/util/MinioUtil.java
@@ -2,7 +2,6 @@ package site.nansan.global.util;
 
 import io.minio.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
